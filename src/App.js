a57b3b6f0f9f9988da38ab4ded3bc97762cf3477
Moh Jaber <51908517+moh-prog@users.noreply.github.com>
2020-07-19 15:40:51 +0300

    intersection-observer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+window.addEventListener("load", () => {
+  let nav = document.getElementById("nav");
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0,
+  };
+  let observer = new IntersectionObserver(observerCallback, options);
+  observer.observe(nav);
+});
+function observerCallback(entries, observer) {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (!entry.intersectionRatio) {
+      document.getElementById("nav").style.position = "sticky";
+      document.getElementById("nav").style.top = 0;
+    } 
+    // else if(entry.intersectionRatio === 1){
+    //   document.getElementById("nav").style.position = "relative";
+    //   document.getElementById('nav').style.removeProperty('top')
+    // }
+  });
+}
 
 function App() {
   return (
     <div className="App">
+      <nav id="nav">
+        <ul>
+          <li>Home</li>
+          <li>More..</li>
+          <li>About US</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +50,65 @@ function App() {
         >
           Learn React
         </a>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
+        <p>asdasdasdasdas</p>
       </header>
     </div>
   );
